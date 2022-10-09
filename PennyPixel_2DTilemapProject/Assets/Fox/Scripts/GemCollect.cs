32e239a428294f251c1fa7d962e0aa8888feec64
MYTEE-MITE <98055258+MYTEE-MITE@users.noreply.github.com>
2022-10-09 06:00:29 -0500

    5A

---
+++ PennyPixel_2DTilemapProject/Assets/Fox/Scripts/GemCollect.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * John Green
+ * Assignment 5A
+ * Handles player collision with gems, win zone, and loss zone. Implements restart functionality.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GemCollect : MonoBehaviour
+{
+    public ScoreManager scoreManager;
+    public bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreManager = FindObjectOfType<Text>().GetComponent<ScoreManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver)
+        {
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Gem"))
+        {
+            scoreManager.score++;
+        }
+
+        if(collision.gameObject.CompareTag("Loss"))
+        {
+            scoreManager.scoreText.text = "You Lose. Press R to Restart.";
+            gameOver = true;
+        }
+
+        if (collision.gameObject.CompareTag("Win"))
+        {
+            scoreManager.scoreText.text = "You Win! Press R to Restart.";
+            gameOver = true;
+        }
+
+    }
+}
---
+++ PennyPixel_2DTilemapProject/Assets/Fox/Scripts/ScoreManager.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * John Green
+ * Assignment 5A
+ * Keeps track of score.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    public float score = 0;
+    public GemCollect gemCollect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = FindObjectOfType<Text>();
+        gemCollect = GameObject.FindGameObjectWithTag("Player").GetComponent<GemCollect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gemCollect.gameOver)
+        {
+            scoreText.text = "Score: " + score;
+        }
+        
+    }
+}
